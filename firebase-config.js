cfa0fc0f969fbe2f7a30ab97de473b9ac7f0a78d
tanyeee <64396512+tanyeee@users.noreply.github.com>
2026-03-25 14:38:13 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  get,
+  push,
+  update,
+  set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4X4s8rG1i62Fe8ypxG29x3ReXSwwGAEo",
+  authDomain: "coffeeroastery-fefa7.firebaseapp.com",
+  databaseURL: "https://coffeeroastery-fefa7-default-rtdb.firebaseio.com",
+  projectId: "coffeeroastery-fefa7",
+  storageBucket: "coffeeroastery-fefa7.firebasestorage.app",
+  messagingSenderId: "854048573547",
+  appId: "1:854048573547:web:92723b65e9ae6ea8a7092e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, onValue, get, push, update, set };
